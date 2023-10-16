c298d2f1125af67c07566ee62ab0183d23e55a09
iershov <iershov@team.amocrm.com>
2023-10-16 22:38:12 +0300

    remove commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,10 @@
 'use client'
-import { useEffect } from 'react'
-import styles from './page.module.css'
-import { collection, doc, getDoc, setDoc } from 'firebase/firestore'; 
-import { db } from '@/firebase';
+import React from 'react';
 import { Input } from 'antd';
 import { MoviesList } from '@/components/MoviesList';
-import React from 'react';
+import styles from './page.module.css'
 
 export default function Home() {
-  // useEffect(() => {
-  //   fetchData()
-  // }, [])
-
-  // const citiesRef = collection(db, "movies");
-
-  // const fetchData = async () => {
-  //   await setDoc(doc(citiesRef, 'haha2'), {
-  //     name: "San Francisco", state: "CA", country: "USA",
-  //     capital: false, population: 860000,
-  //     regions: ["west_coast", "norcal"] });
-  // }
-  // const [result, setResult] = React.useState()
-  // const ref = doc(db, 'movies', 'ee1b35cf-bea3-4cd9-a1ee-d0dd1b8a59da');
-  // async function docSnap() {
-  //   const data = await getDoc(ref);
-  //   setResult(data.data())
-  // }
-
-  // useEffect(() => {
-  //   docSnap()
-  // }, [])
-
-  // console.log(result)
-  
-
   return (
     <main className={styles.main}>
       <Input.Search 
